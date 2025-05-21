0d1c10111b1963a66cc3ebd2121cb8bb916092e8
= <=>
2025-05-21 13:31:31 -0400

    events are now listed in the venue popup card

--- VenueFrontend/src/app/components/venue-popup/venue-popup.component.ts
+++ VenueFrontend/src/app/components/venue-popup/venue-popup.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Event } from '../../models/events';
+import { EventService } from '../../services/event.service';
 @Component({
   selector: 'app-venue-popup',
   standalone: true,
@@ -9,8 +11,20 @@ import { CommonModule } from '@angular/common';
 })
 export class VenuePopupComponent {
   @Input() venue!: any;
+  @Input() events: Event[] = [];
   @Input() position: { x: number; y: number } | null = null;
   @Output() close = new EventEmitter<void>();
-  viewEvents() {
-}
+  showEvents = false;
+
+  constructor(private eventService: EventService) {}
+
+  viewEvents(): void {
+    this.showEvents = !this.showEvents;
+
+    if (this.showEvents && this.events.length === 0 && this.venue.eventIds?.length) {
+      this.eventService.getEventsByIds(this.venue.eventIds).subscribe(events => {
+        this.events = events;
+      });
+    }
+  }
 }
