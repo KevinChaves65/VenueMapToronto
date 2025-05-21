0a5ea8ef985c2c15cfc8640555c32d41dc9b74ed
= <=>
2025-05-21 14:56:55 -0400

    got a more info button to display full details of an event

--- VenueFrontend/src/app/components/venue-popup/venue-popup.component.ts
+++ VenueFrontend/src/app/components/venue-popup/venue-popup.component.ts
@@ -2,10 +2,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Event } from '../../models/events';
 import { EventService } from '../../services/event.service';
+import { EventCardComponent } from '../side-bar-collection/event-card/event-card.component';
 @Component({
   selector: 'app-venue-popup',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, EventCardComponent],
   templateUrl: './venue-popup.component.html',
   styleUrl: './venue-popup.component.css'
 })
@@ -15,7 +16,7 @@ export class VenuePopupComponent {
   @Input() position: { x: number; y: number } | null = null;
   @Output() close = new EventEmitter<void>();
   showEvents = false;
-
+  selectedEvent: Event | null = null;
   constructor(private eventService: EventService) {}
 
   viewEvents(): void {
@@ -27,4 +28,12 @@ export class VenuePopupComponent {
       });
     }
   }
+
+openEvent(event: Event): void {
+  this.selectedEvent = event;
+}
+
+closeEvent = (): void => {
+  this.selectedEvent = null;
+};
 }
