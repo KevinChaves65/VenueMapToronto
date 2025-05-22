25b46c765f136e01c96b853e7fc400aef04de9c8
= <=>
2025-05-22 15:31:39 -0400

    just tidy up files and comments

--- VenueFrontend/src/app/components/side-bar-collection/venue-card/venue-card.component.ts
+++ VenueFrontend/src/app/components/side-bar-collection/venue-card/venue-card.component.ts
@@ -3,14 +3,13 @@ import { CommonModule } from '@angular/common';
 import { Venue } from '../../../models/venues';
 import { Event } from '../../../models/events';
 import { EventService } from '../../../services/event.service';
-import { EventCardComponent } from '../event-card/event-card.component';
 import { MapService } from '../../../services/map.service';
 
 
 @Component({
   selector: 'app-venue-card',
   standalone: true,
-  imports: [CommonModule, EventCardComponent],
+  imports: [CommonModule],
   templateUrl: './venue-card.component.html',
   styleUrls: ['./venue-card.component.css']
 })
