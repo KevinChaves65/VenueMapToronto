df5fd9b7c00abc51b07c2564d944a2eb490a3ca2
= <=>
2025-09-18 18:22:19 -0400

    pretty up one more time

--- VenueFrontend/src/app/pages/landing-page/landing-page.component.ts
+++ VenueFrontend/src/app/pages/landing-page/landing-page.component.ts
@@ -1,9 +1,9 @@
 import { Component, computed, signal, effect, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-landing-page',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -12,6 +12,9 @@ export class LandingPageComponent {
 readonly images = ['concert1.jpg', 'concert2.jpg', 'concert3.jpg', 
   'concert4.jpg','concert5.jpg', 'concert6.jpg', 'concert7.jpg', 'concert8.jpg', 'concert9.jpg'];
 
+readonly genres = ['Rock', 'Pop', 'Hip-Hop', 'Jazz', 'Electronic', 'Classical'];
+
+maxPrice = 300;
   private index = signal(0);
 
   // Expose index via computed for template binding
