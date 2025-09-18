aa4522872f27ed279d2e2d384badf0828a83ca62
= <=>
2025-09-18 16:16:51 -0400

    landing page made, gotta make it look nice now

--- VenueFrontend/src/app/app.component.ts
+++ VenueFrontend/src/app/app.component.ts
@@ -3,14 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { MapViewComponent } from './components/map-view/map-view.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SideBarComponent, MapViewComponent, TopBarComponent],
+  imports: [RouterOutlet, SideBarComponent, MapViewComponent, TopBarComponent, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'VenueFrontend';
+  title = 'Showfari';
 }
