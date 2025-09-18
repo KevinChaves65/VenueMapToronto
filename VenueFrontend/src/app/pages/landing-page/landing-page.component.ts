41721eeec304103323bb8e357405d9c7f77b5cdb
= <=>
2025-09-18 19:47:05 -0400

    leaving notes...

--- VenueFrontend/src/app/pages/landing-page/landing-page.component.ts
+++ VenueFrontend/src/app/pages/landing-page/landing-page.component.ts
@@ -10,10 +10,10 @@ import { Router } from '@angular/router';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LandingPageComponent {
-
+  //change this later to pull from a collection from the backend
   readonly images = ['concert1.jpg', 'concert2.jpg', 'concert3.jpg', 
   'concert4.jpg','concert5.jpg', 'concert6.jpg', 'concert7.jpg', 'concert8.jpg', 'concert9.jpg'];
-
+  //change later to pull from event.genres for better queries
   readonly genres = ['Rock', 'Pop', 'Hip-Hop', 'Jazz', 'Electronic', 'Classical'];
 
   maxPrice = 300;
