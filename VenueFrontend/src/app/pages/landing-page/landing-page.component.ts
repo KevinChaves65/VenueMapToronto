b073559de5847e5d3bffa6df11fd0cbf5e1d6749
= <=>
2025-09-18 17:23:40 -0400

    looks prettish functionality and routing needs to be done now

--- VenueFrontend/src/app/pages/landing-page/landing-page.component.ts
+++ VenueFrontend/src/app/pages/landing-page/landing-page.component.ts
@@ -1,12 +1,26 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, computed, signal, effect, ChangeDetectionStrategy } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LandingPageComponent {
+readonly images = ['concert1.jpg', 'concert2.jpg', 'concert3.jpg', 
+  'concert4.jpg','concert5.jpg', 'concert6.jpg', 'concert7.jpg', 'concert8.jpg', 'concert9.jpg'];
 
+  private index = signal(0);
+
+  // Expose index via computed for template binding
+  currentIndex = computed(() => this.index());
+
+  constructor() {
+    // Auto-advance every 6 seconds
+    setInterval(() => {
+      this.index.update(i => (i + 1) % this.images.length);
+    }, 6000);
+  }
 }
