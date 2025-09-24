709acbfe839c44db85960a2caebd74403a811a80
= <=>
2025-09-24 19:19:07 -0400

    updated member attributes

--- VenueFrontend/src/app/pages/about-page/about-page.component.ts
+++ VenueFrontend/src/app/pages/about-page/about-page.component.ts
@@ -13,11 +13,11 @@ export class AboutPageComponent implements OnInit {
   missionHtml: string = '';
 
   team = [
-    { name: 'Kevin Chaves', role: 'Co Founder/Software Developer', LinkedinUrl: "", img: 'assets/images/team/kevin.jpg' },
-    { name: 'Daniel A', role: 'Co Founder/Outreach', LinkedinUrl: "", img: 'assets/images/team/dan.jpg' },
-    { name: 'Adolfo David Romero', role: 'Software Developer', LinkedinUrl: "", img: 'assets/images/team/david.jpg' },
-    { name: 'Evan', role: 'Software Developer', LinkedinUrl: "", img: 'assets/images/team/evan.jpg' },
-    { name: 'Rani', role: 'UI Designer', LinkedinUrl: "", img: 'assets/images/team/rani.jpg' },
+    { name: 'Kevin Chaves', role: 'Co Founder/Software Developer', LinkedinUrl: "https://www.youtube.com/watch?v=PDJLvF1dUek", img: 'assets/images/team/kevin.jpg' },
+    { name: 'Daniel A', role: 'Co Founder/Outreach', LinkedinUrl: "https://www.youtube.com/watch?v=PDJLvF1dUek", img: 'assets/images/team/dan.jpg' },
+    { name: 'Adolfo David Romero', role: 'Software Developer', LinkedinUrl: "https://www.youtube.com/watch?v=PDJLvF1dUek", img: 'assets/images/team/david.jpg' },
+    { name: 'Evan', role: 'Software Developer', LinkedinUrl: "https://www.youtube.com/watch?v=PDJLvF1dUek", img: 'assets/images/team/evan.jpg' },
+    { name: 'Rani', role: 'UI Designer', LinkedinUrl: "https://www.youtube.com/watch?v=PDJLvF1dUek", img: 'assets/images/team/rani.jpg' },
   ];
 
   constructor(private http: HttpClient) {}
