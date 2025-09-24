be0d69275936b2f2d3c807ccf9376cc92f9f357d
= <=>
2025-09-24 00:01:14 -0400

    about page added, gonna pretty up now

--- VenueFrontend/src/app/app.routes.ts
+++ VenueFrontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { EventCardComponent } from './components/side-bar-collection/event-card/event-card.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { MapPageComponent } from './pages/map-page/map-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
 
 export const routes: Routes = [
     { path: 'events/:title', component: EventCardComponent},
     { path: '', component: LandingPageComponent },
-    { path: 'map', component: MapPageComponent }
+    { path: 'map', component: MapPageComponent },
+    { path: 'about', component: AboutPageComponent}
 ];
---
+++ VenueFrontend/src/app/pages/about-page/about-page.component.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-about-page',
+  imports: [CommonModule],
+  templateUrl: './about-page.component.html',
+  styleUrl: './about-page.component.css'
+})
+export class AboutPageComponent implements OnInit {
+  aboutHtml: string = '';
+  missionHtml: string = '';
+
+  team = [
+    { name: 'Kevin Chaves', role: 'Co Founder/Software Developer', LinkedinUrl: "", img: 'assets/images/team/kevin.jpg' },
+    { name: 'Daniel A', role: 'Co Founder/Outreach', LinkedinUrl: "", img: 'assets/images/team/dan.jpg' },
+    { name: 'Adolfo David Romero', role: 'Software Developer', LinkedinUrl: "", img: 'assets/images/team/david.jpg' },
+    { name: 'Evan', role: 'Software Developer', LinkedinUrl: "", img: 'assets/images/team/evan.jpg' },
+    { name: 'Rani', role: 'UI Designer', LinkedinUrl: "", img: 'assets/images/team/rani.jpg' },
+  ];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('assets/data/about.md', { responseType: 'text' }).subscribe(data => this.aboutHtml = data);
+    this.http.get('assets/data/mission.md', { responseType: 'text' }).subscribe(data => this.missionHtml = data);
+    };
+}
