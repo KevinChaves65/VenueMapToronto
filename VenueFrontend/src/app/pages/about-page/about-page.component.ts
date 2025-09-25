6b48ba739aa8fc73379c07e23f38a20a25916967
= <=>
2025-09-25 19:51:47 -0400

    opps forgor dans full name

--- VenueFrontend/src/app/pages/about-page/about-page.component.ts
+++ VenueFrontend/src/app/pages/about-page/about-page.component.ts
@@ -14,7 +14,7 @@ export class AboutPageComponent implements OnInit {
 
   team = [
     { name: 'Kevin Chaves', role: 'Co Founder/Software Developer', LinkedinUrl: "https://www.youtube.com/watch?v=PDJLvF1dUek", img: 'assets/images/team/kevin.jpg' },
-    { name: 'Daniel A', role: 'Co Founder/Outreach', LinkedinUrl: "https://www.youtube.com/watch?v=PDJLvF1dUek", img: 'assets/images/team/dan.jpg' },
+    { name: 'Daniel Arceo', role: 'Co Founder/Outreach', LinkedinUrl: "https://www.youtube.com/watch?v=PDJLvF1dUek", img: 'assets/images/team/dan.jpg' },
     { name: 'Adolfo David Romero', role: 'Software Developer', LinkedinUrl: "https://www.youtube.com/watch?v=PDJLvF1dUek", img: 'assets/images/team/david.jpg' },
     { name: 'Evan', role: 'Software Developer', LinkedinUrl: "https://www.youtube.com/watch?v=PDJLvF1dUek", img: 'assets/images/team/evan.jpg' },
     { name: 'Rani', role: 'UI Designer', LinkedinUrl: "https://www.youtube.com/watch?v=PDJLvF1dUek", img: 'assets/images/team/rani.jpg' },
