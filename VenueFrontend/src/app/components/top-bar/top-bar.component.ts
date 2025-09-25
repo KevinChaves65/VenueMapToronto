3efec3e691b298bb216cdfa64d09001b83122ef2
= <=>
2025-09-24 23:59:32 -0400

    added it to map section

--- VenueFrontend/src/app/components/top-bar/top-bar.component.ts
+++ VenueFrontend/src/app/components/top-bar/top-bar.component.ts
@@ -2,23 +2,26 @@ import { Component, ElementRef, HostListener, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { AboutComponent } from '../top-bar-collection/about/about.component';
+import { AboutPageComponent } from '../../pages/about-page/about-page.component';
 import { ProfileComponent } from '../top-bar-collection/profile/profile.component';
 import { SettingsComponent } from '../top-bar-collection/settings/settings.component';
 
 @Component({
   selector: 'app-top-bar',
-  imports: [CommonModule, MatButtonModule, MatIconModule, AboutComponent, SettingsComponent, ProfileComponent],
+  imports: [CommonModule, MatButtonModule, MatIconModule, AboutPageComponent, SettingsComponent, ProfileComponent],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.css'
 })
 export class TopBarComponent implements OnDestroy{
-activePanel: 'social' | 'settings' | 'account' | null = null;
+activePanel: 'about' | 'settings' | 'account' | null = null;
 constructor(private eRef: ElementRef) {}
 
-  togglePanel(panel: 'social' | 'settings' | 'account'): void {
+  togglePanel(panel: 'about' | 'settings' | 'account'): void {
     this.activePanel = this.activePanel === panel ? null : panel;
   }
+  closePanel() {
+  this.activePanel = null;
+}
   @HostListener('document:click', ['$event'])
   handleOutsideClick(event: MouseEvent): void {
     if (!this.eRef.nativeElement.contains(event.target)) {
