7d559ca5035b71cd9f51818e449268d0e5115b35
Rani Sofya <rani.sofya@trulycustom.ca>
2025-11-08 20:57:38 -0500

    Update styles and components for improved layout and structure
    - Added `div` to text styles in SCSS for broader application of secondary text color.
    - Replaced existing content in `+page.svelte` with a footer component for better structure and maintainability.

---
+++ Svelte/src/lib/componants/footer.svelte
@@ -0,0 +1,96 @@
+<link
+  rel="preload"
+  as="image"
+  href="https://d22po4pjz3o32e.cloudfront.net/logo-image.svg"
+/>
+<footer id="showfari" class="px-[5%] py-12 md:py-18 lg:py-20">
+  <div class="container">
+    <div
+      class="grid grid-cols-1 items-start justify-between gap-x-[8vw] gap-y-12 pb-12 sm:gap-y-10 md:gap-y-14 md:pb-18 lg:grid-cols-[1fr_0.5fr] lg:pb-20"
+    >
+      <div
+        class="flex flex-col items-start"
+      >
+        <a href="#" class="mb-8"
+          ><img
+            src="https://d22po4pjz3o32e.cloudfront.net/logo-image.svg"
+            alt="Logo image"
+            class="inline-block invert"
+        /></a>
+        <ul
+          class="grid grid-flow-row grid-cols-1 items-start justify-center justify-items-start gap-y-4 md:grid-flow-col md:grid-cols-[max-content] md:justify-start md:justify-items-start md:gap-x-6"
+        >
+          <li class="font-semibold">
+            <a href="#">Home</a>
+          </li>
+          <li class="font-semibold">
+            <a href="#">Events</a>
+          </li>
+          <li class="font-semibold">
+            <a href="#">About</a>
+          </li>
+          <li class="font-semibold">
+            <a href="#">Contact</a>
+          </li>
+          <li class="font-semibold">
+            <a href="#">Blog</a>
+          </li>
+        </ul>
+      </div>
+      <div
+        class="max-w-md lg:min-w-[25rem]"
+      >
+        <p class="mb-3 font-semibold md:mb-4">
+          Subscribe
+        </p>
+        <form
+          class="mb-3 grid grid-cols-1 gap-x-4 gap-y-3 sm:grid-cols-[1fr_max-content] sm:gap-y-4 md:gap-4"
+        >
+          <div class="relative flex w-full items-center">
+            <input
+              type="email"
+              class="input"
+              id="email"
+              placeholder="Enter your email"
+              value=""
+            />
+          </div>
+          <button
+            class="secondary"
+            title="Subscribe"
+          >
+            Subscribe
+          </button>
+        </form>
+        <p class="text-xs">
+          By subscribing you agree to with our Privacy Policy
+        </p>
+      </div>
+    </div>
+    <div class="h-px w-full bg-secondary-dark"></div>
+    <div
+      class="flex flex-col items-start justify-start pb-4 pt-6 text-sm md:flex-row md:items-center md:justify-between md:pb-0 md:pt-8 md:text-center"
+    >
+      <ul
+        class="grid grid-flow-row grid-cols-[max-content] gap-y-4 text-sm md:grid-flow-col md:gap-x-6 md:gap-y-0 lg:justify-center"
+      >
+        <li
+          class="underline decoration-black underline-offset-1"
+        >
+          <a href="#">Privacy Policy</a>
+        </li>
+        <li
+          class="underline decoration-black underline-offset-1"
+        >
+          <a href="#">Terms of Service</a>
+        </li>
+        <li
+          class="underline decoration-black underline-offset-1"
+        >
+          <a href="#">Cookies Settings</a>
+        </li>
+      </ul>
+      <p>© 2025 Showfari. All rights reserved.</p>
+    </div>
+  </div>
+</footer>
--- Svelte/src/routes/+page.svelte
+++ Svelte/src/routes/+page.svelte
@@ -1,11 +1,5 @@
+<script lang="ts">
+	import Footer from '$lib/componants/footer.svelte';
+</script>
 
-
-<h1 class="text-2xl font-bold text-primary">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-<button class="large secondary">Click me</button>
-
-<div class="card-lg inline-flex gap-2 items-center justify-center w-auto">
-    <h2>Card Title</h2>
-    <p>Card Description</p>
-</div>
+<Footer />
